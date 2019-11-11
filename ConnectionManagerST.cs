680701c87c71c67bd8140cbf28ed37b55aa43b24
chronosWorker <bnemeth@chronossystems.com>
2019-11-11 11:40:36 +0100

    add Singleton ConnectionManagerST class

---
+++ ConnectionManagerST.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+
+namespace Process_Export_Import
+{
+    public class ConnectionManagerST
+    {
+
+        public static ConnectionManagerST _instance;
+        public static readonly object _padlock;
+
+        private ConnectionManagerST()
+        {
+
+        }
+
+        public static ConnectionManagerST Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+
+                    lock (_padlock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new ConnectionManagerST();
+                        }
+                    }
+
+                }
+
+                return _instance;
+            }
+        }
+
+        public string connectionStringToDbFile { get; set; } = ConfigurationManager.ConnectionStrings["Default"].ToString();
+        SQLiteConnection connSqlite;
+
+        public string connectionStringToSqlServer { get; set; } = ConfigurationManager.AppSettings.Get("connstrRe");
+        SqlConnection connection;
+
+
+
+        public void openSqLiteConnection()
+        {
+            connSqlite = new SQLiteConnection(connectionStringToDbFile);
+            connSqlite.Open();
+        }
+
+        public void executeQueriesInDbFile(string Query_)
+        {
+            SQLiteCommand cmdTxt = new SQLiteCommand(Query_, connSqlite);
+            cmdTxt.ExecuteNonQuery();
+        }
+
+        public SQLiteDataReader sqLiteDataReader(string Query_)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(Query_, connSqlite);
+            SQLiteDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+        public void closeSqLiteConnection()
+        {
+            connSqlite.Close();
+        }
+
+
+
+        public void openSqlServerConnection()
+        {
+            connection = new SqlConnection(connectionStringToSqlServer);
+            connection.Open();
+
+        }
+
+        public void executeQueriesInSqlServer(string Query_)
+        {
+            SqlCommand cmdText = new SqlCommand(Query_, connection);
+            cmdText.ExecuteNonQuery();
+        }
+
+        public SqlDataReader DataReader(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, connection);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+        public void closeSqlServerConnection()
+        {
+            connection.Close();
+        }
+    }
+
+}
